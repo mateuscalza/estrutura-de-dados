ac46797e4627d697a5cf6577d3df9b60c7bbf67b
Mateus Calza <mateushenrique17@gmail.com>
2019-07-30 19:47:03 -0300

    Iniciando com a primeira lista de exercícios.

---
+++ aula-1/atividade-1.c
@@ -0,0 +1,18 @@
+/**
+ * Soma
+ *
+ * @author Mateus Calza
+**/
+
+#include <stdio.h>
+
+double soma(double valor1, double valor2)
+{
+  return valor1 + valor2;
+}
+
+int main()
+{
+  printf("Soma de dois valores: \n");
+  printf("Resultado: %.2f", soma(123, 456));
+}
