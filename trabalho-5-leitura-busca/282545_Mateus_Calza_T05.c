909a626ecb51474eafb136843ffe6552765457dd
Mateus Calza <mateushenrique17@gmail.com>
2019-10-09 11:27:57 -0300

    Menu e cadastro sem validação.

--- trabalho-5-leitura-busca/282545_Mateus_Calza_T05.c
+++ trabalho-5-leitura-busca/282545_Mateus_Calza_T05.c
@@ -10,36 +10,14 @@
 
 struct registro
 {
-  int codigo;
-  int idade;
+  int id;
   char nome[80];
+  char telefone[12];
+  int dia;
+  int mes;
+  int ano;
 };
 typedef struct registro Registro;
-
-char *randstring(size_t length)
-{
-
-  static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
-  char *randomString = NULL;
-
-  if (length)
-  {
-    randomString = malloc(sizeof(char) * (length + 1));
-
-    if (randomString)
-    {
-      for (int n = 0; n < length; n++)
-      {
-        int key = rand() % (int)(sizeof(charset) - 1);
-        randomString[n] = charset[key];
-      }
-
-      randomString[length] = '\0';
-    }
-  }
-
-  return randomString;
-}
 
 void ordenaNomeCrescente(Registro *registros, int tamanho)
 {
@@ -60,62 +38,36 @@ void ordenaNomeCrescente(Registro *registros, int tamanho)
   }
 }
 
-void ordenaCodigoCrescente(Registro *registros, int tamanho)
+void atualizaArquivo(Registro *registros, int tamanho)
 {
-  int a, b;
-  Registro troca;
-
-  for (a = 1; a < tamanho; a++)
-  {
-    for (b = tamanho - 1; b >= a; b--)
-    {
-      if (registros[b - 1].codigo > registros[b].codigo)
-      {
-        troca = registros[b - 1];
-        registros[b - 1] = registros[b];
-        registros[b] = troca;
-      }
-    }
-  }
-}
-
-void ordenaIdadeDecrescente(Registro *registros, int tamanho)
-{
-  int a, b;
-  Registro troca;
-
-  for (a = 1; a < tamanho; a++)
-  {
-    for (b = tamanho - 1; b >= a; b--)
-    {
-      if (registros[b - 1].idade < registros[b].idade)
-      {
-        troca = registros[b - 1];
-        registros[b - 1] = registros[b];
-        registros[b] = troca;
-      }
-    }
-  }
 }
 
 void insereRegistro(Registro *registros, int tamanho)
 {
+  // ID
+  printf("Digite o ID: \n");
+  scanf("%d", &registros[tamanho].id);
+
+  // Nome
   printf("Digite o nome: \n");
   fgets(registros[tamanho].nome, 80, stdin);
   int size = strlen(registros[tamanho].nome);
   registros[tamanho].nome[size - 1] = '\0';
   scanf(" %[^\t\n]s", registros[tamanho].nome);
-  printf("Digite o código: \n");
-  scanf("%d", &registros[tamanho].codigo);
-  printf("Digite a idade: \n");
-  scanf("%d", &registros[tamanho].idade);
-}
 
-void insereRegistroAleatorio(Registro *registros, int tamanho)
-{
-  registros[tamanho].codigo = rand() % 100;
-  registros[tamanho].idade = rand() % 100 + 1;
-  strcpy(registros[tamanho].nome, randstring(80));
+  // Telefone
+  printf("Digite o telefone: \n");
+  fgets(registros[tamanho].telefone, 12, stdin);
+  size = strlen(registros[tamanho].telefone);
+  registros[tamanho].telefone[size - 1] = '\0';
+  scanf(" %[^\t\n]s", registros[tamanho].telefone);
+
+  // Data de nascimento
+  printf("Digite a data de nascimento: \n");
+  scanf("%d/%d/%d", &registros[tamanho].dia, &registros[tamanho].mes, &registros[tamanho].ano);
+
+  // Atualiza arquivo
+  atualizaArquivo(registros, tamanho + 1);
 }
 
 void listar(Registro *registros, int tamanho)
@@ -123,7 +75,15 @@ void listar(Registro *registros, int tamanho)
   printf("Registros: \n");
   for (int indice = 0; indice < tamanho; indice++)
   {
-    printf("Código %d - %d anos - %s\n", registros[indice].codigo, registros[indice].idade, registros[indice].nome);
+    printf(
+      "\nCódigo: %d\nNome: %s\nTelefone: %s\nData de nascimento: %d/%d/%d\n",
+      registros[indice].id,
+      registros[indice].nome,
+      registros[indice].telefone,
+      registros[indice].dia,
+      registros[indice].mes,
+      registros[indice].ano
+    );
   }
   printf("\n");
 }
@@ -133,21 +93,17 @@ void menu()
   Registro registros[50];
   int tamanho = 0;
 
-  for (int indice = 0; indice < 10; indice++)
-  {
-    insereRegistroAleatorio(registros, tamanho);
-    tamanho++;
-  }
-
   int opcao;
   do
   {
     printf("-------------------------\n");
     printf("Escolha uma opção:\n");
     printf("1 - Inserir registro\n");
-    printf("2 - Ordenar em ordem crescente por código\n");
-    printf("3 - Ordenar em ordem crescente por nome\n");
-    printf("4 - Ordenar em ordem descrescente por idade\n");
+    printf("2 - Remover contato\n");
+    printf("3 - Pesquisar pelo nome\n");
+    printf("4 - Listar todos\n");
+    printf("5 - Listar aniversariantes do mês\n");
+    printf("6 - Listar contatos pela inicial\n");
     printf("9 - Sair\n");
     printf("-------------------------\n");
     scanf("%d", &opcao);
@@ -160,18 +116,8 @@ void menu()
       tamanho++;
       break;
 
-    case 2:
-      ordenaCodigoCrescente(registros, tamanho);
-      listar(registros, tamanho);
-      break;
-
-    case 3:
-      ordenaNomeCrescente(registros, tamanho);
-      listar(registros, tamanho);
-      break;
-
     case 4:
-      ordenaIdadeDecrescente(registros, tamanho);
+      ordenaNomeCrescente(registros, tamanho);
       listar(registros, tamanho);
       break;
 
